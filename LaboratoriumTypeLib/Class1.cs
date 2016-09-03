c3d64184d630265150648e872aa5c8e22084ec05
Vinogradov Kirill <vikian050194@gmail.com>
2016-09-03 18:07:26 +0300

    add projects with lib's

---
+++ LaboratoriumTypeLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoriumTypeLib
+{
+    public class Class1
+    {
+    }
+}
