0ad6e4ef9e0ed62f658d90afd4878b5808198d01
Vinogradov Kirill <vikian050194@gmail.com>
2016-11-11 00:11:04 +0300

    Add sample with subalgorithm

--- Sandbox.FSharp/Script.fsx
+++ Sandbox.FSharp/Script.fsx
@@ -1,5 +1,7 @@
-﻿//#r @"D:\Code\MVC\Laboratorium\Laboratorium.Algorithms\bin\Debug\Laboratorium.Algorithms.dll"
-#r @"C:\Users\KirillV\Documents\Git\Laboratorium\Laboratorium.Algorithms\bin\Debug\Laboratorium.Algorithms.dll"
-open Laboratorium.Algorithms.Test
+﻿#r @"D:\Code\MVC\Laboratorium\Laboratorium.Algorithms\bin\Debug\Laboratorium.Algorithms.dll"
+//#r @"C:\Users\KirillV\Documents\Git\Laboratorium\Laboratorium.Algorithms\bin\Debug\Laboratorium.Algorithms.dll"
 open Laboratorium.Algorithms.Factorization
-let foo x = Bar().Execute()
+open Laboratorium.Algorithms
+let gcd a b = SingleGCD().Execute(a,b)
+let gcdInstance(g) = { new IAlgorithmS<int32> with member this.Execute(a,b) = g a b }
+let lcm a b g = SingleLCM().Execute(a,b,gcdInstance(g))
