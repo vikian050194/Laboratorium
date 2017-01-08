05f87269251a9245d69584b72555b4c426d52df0
Vinogradov Kirill <vikian050194@gmail.com>
2017-01-08 17:50:19 +0300

    add advanced euclidean algorithm

---
+++ Laboratorium.Algorithms/Factorization/Common/EuclideanAlgorithm/TableRow.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Laboratorium.Algorithms.Factorization.Common.EuclideanAlgorithm
+{
+    public class TableRow
+    {
+        public BigInteger U { get; set; }
+        public BigInteger V { get; set; }
+        public BigInteger R { get; set; }
+        public BigInteger Q { get; set; }
+    }
+}
